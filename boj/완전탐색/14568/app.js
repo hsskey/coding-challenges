ba412b369226af46efb9a66881bf59c4c0089415
hsskey <slingshot800@gmail.com>
2024-02-18 17:51:48 +0900

    chore: init

--- boj/완전탐색/14568/app.js
+++ boj/완전탐색/14568/app.js
@@ -4,7 +4,7 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/inp
 const input = fs.readFileSync(filePath).toString().trim().split('\n')
 
 const N = parseInt(input, 10)
-const cnt = 0
+let cnt = 0
 
 for(let i = 1; i <= N; i++) {
     for(let j = 1; j <= N; j++) {
