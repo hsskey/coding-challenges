3d83695080b278a4f16d968e16f72ad1d28167ad
hsskey <slingshot800@gmail.com>
2024-03-11 23:45:43 +0900

    chore: leetcode 문제풀이 추가

---
+++ coding_moon/leetcode/runningSum.js
@@ -0,0 +1,44 @@
+/**
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var runningSum = function(nums) {
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1]
+    }
+    return nums
+    // reduce를 이용한 풀이
+    // const result = []
+    // result.push(nums[0])
+    // nums.reduce((acc, current) => {
+    //     result.push(acc + current)
+    //     return acc + current
+    // })
+    // return result
+};
+
+console.log(runningSum([3,1,2,10,1]))
