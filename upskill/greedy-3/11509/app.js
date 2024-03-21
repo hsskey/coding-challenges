45e008940ce41256dec7e11424f8141c02de987c
hsskey <slingshot800@gmail.com>
2024-03-22 00:09:40 +0900

    chore: 코드정리

--- upskill/greedy-3/11509/app.js
+++ upskill/greedy-3/11509/app.js
@@ -4,16 +4,15 @@ const input = fs.readFileSync(filePath).toString().split('\n')
 
 let data = input[1].split(' ').map(Number)
 let result = 0
-let arrow = new Array(1000001).fill(0)
+let arrow = new Array(1000001).fill(0) // 각 높이에 화살이 몇개있는지 기록용 배열
 
-for(let x of data) {
-    if(arrow[x] > 0) {
-        console.log('2')
-        arrow[x] -= 1
-        arrow[x - 1] += 1
-    } else {
-        console.log('1')
-        arrow[x - 1] += 1
+for(let item of data) {
+    console.log(arrow);
+    if(arrow[item] > 0) { // 해당 높이에 화살이 있다면
+        arrow[item] -= 1
+        arrow[item - 1] += 1
+    } else { //해당 높이에 화살이 없다면
+        arrow[item - 1] += 1
         result += 1
     }
 }
