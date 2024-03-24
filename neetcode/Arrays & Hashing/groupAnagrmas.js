3c4cd6be9a880c84b9e08323f7fdc3572bff3788
hsskey <slingshot800@gmail.com>
2024-03-25 08:34:54 +0900

    chore: 문제풀이 추가

---
+++ neetcode/Arrays & Hashing/groupAnagrmas.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+ var groupAnagrams = function(strs) {
+    const map = {};
+    for (const str of strs) {
+        const sorted = str.split('').sort().join('');
+        if (!map[sorted]) {
+            map[sorted] = [];
+        }
+        map[sorted].push(str);
+    }
+    return Object.values(map);
+};
