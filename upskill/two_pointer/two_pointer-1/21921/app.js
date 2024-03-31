55a80a23e5d705de9790c663789b52eb1f4c1471
hsskey <slingshot800@gmail.com>
2024-03-31 13:41:39 +0900

    chore: 문제풀이 추가

---
+++ upskill/two_pointer/two_pointer-1/21921/app.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`
+const input = fs.readFileSync(filePath).toString().split('\n')
+const firstLine = input[0].split(' ').map(Number)
+const secondLine= input[1].split(' ').map(Number)
+
+const [n, x] = firstLine // 5, 2
+const data = secondLine
+
+let maxVisit = 0 // 2일동안 최대 방문자수
+let cnt = 1 // 기간이 몇개인지
+
+let start = 0
+let end = 0
+let temp = 0
+for(let start = 0; start < data.length; start++) {
+    while((end < start + x) && (end < data.length)) {
+        temp += data[end] 
+        end += 1
+    }   
+    if(temp === maxVisit) {
+        cnt += 1
+    }
+    maxVisit = Math.max(temp, maxVisit)
+    temp -= data[start]
+}
+
+if(maxVisit === 0) {
+    console.log('SAD')
+} else {
+    console.log(maxVisit)
+    console.log(cnt)
+}
